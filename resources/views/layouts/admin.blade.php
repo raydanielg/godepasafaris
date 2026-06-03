2064eee9240b8e39e2d72ba5a258fad4a7add904
raydanielg <airezra2+1@gmail.com>
2026-06-03 23:27:20 +0300

    commit

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -56,6 +56,7 @@
         @yield('content')
     </div>
 
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     @stack('scripts')
 </body>
 </html>
